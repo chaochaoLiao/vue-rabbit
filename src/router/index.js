c7226dfd125b1647e54f6497b829e4de86c52852
chaochaoLiao <2835201209@qq.com>
2023-08-24 22:43:01 +0800

    daily commit

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Layout from '@/views/Layout/index.vue'
 import Home from '@/views/Home/index.vue'
 import Category from '@/views/Category/index.vue'
 import SubCategory from '@/views/SubCategory/index.vue'
+import Detail from '@/views/Detail/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -19,9 +20,14 @@ const router = createRouter({
         {
           path: '/category/:id',
           component: Category
-        },{
+        },
+        {
           path: '/category/sub/:id',
           component: SubCategory
+        },
+        {
+          path: '/detail/:id',
+          component: Detail
         }
       ]
     },
@@ -30,7 +36,13 @@ const router = createRouter({
       component: Login
     }
 
-  ]
+  ],
+  //路由行为定制
+  scrollBehavior () {
+    return {
+      top: 0,
+    }
+  }
 })
 
 export default router
